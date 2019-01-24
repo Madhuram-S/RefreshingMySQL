711eb30628b349e8c962a09d892706bcc5ba6955
Madhuram Swaminathan <madhuananth04@icloud.com>
2019-01-24 13:34:53 -0600

    Updated the sql file

--- mySQLrefresher_queries.sql
+++ mySQLrefresher_queries.sql
@@ -97,5 +97,114 @@
  INNER JOIN city as c ON a.city_id = c.city_id;
  
  -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+ -- SELECT * FROM payment;
  
- select count(*) from payment;
+ SELECT CONCAT_WS(" ",s.first_name, s.last_name) as `Staff`, SUM(p.amount) as `Total Amount`
+ FROM payment as p
+ INNER JOIN staff as s ON s.staff_id = p.staff_id
+ WHERE p.payment_date BETWEEN DATE('2005-08-01 00:00:00') AND DATE('2005-08-31 11:59:59')
+ GROUP BY p.staff_id;
+ 
+ -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film
+ SELECT f.title as `Film`, COUNT(fa.actor_id) as `Num. of Actors`
+ FROM film as f
+ INNER JOIN film_actor as fa ON f.film_id = fa.film_id
+ GROUP BY f.film_id;
+ 
+ -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+ SELECT f.title as Title, COUNT(i.film_id) as `# of Copies`
+ FROM inventory as i
+ INNER JOIN film as f ON i.film_id = f.film_id
+ WHERE UCASE(title) = UCASE("Hunchback Impossible");
+ -- GROUP BY f.title ORDER BY COUNT(i.film_id) DESC ;
+ 
+ -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
+ -- List the customers alphabetically by last name:
+ 
+ SELECT c.first_name as `First Name`, c.last_name as `Last Name`, SUM(p.amount) as `Total Amount Paid`
+ FROM customer as c
+ INNER JOIN payment as p ON c.customer_id = p.customer_id
+ GROUP BY c.customer_id ORDER BY c.last_name;
+ 
+ -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence.
+-- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
+ -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+ 
+
+SELECT f.title as `Title` FROM film as f
+WHERE language_id = (SELECT language_id FROM language WHERE UCASE(name) = UCASE("English")) AND 
+(UCASE(f.title) LIKE UCASE("Q%") OR UCASE(f.title) LIKE UCASE("K%"));
+
+-- 7b. Use subqueries to display all actors who appear in the film Alone Trip
+
+SELECT CONCAT_WS(" ",a.first_name, a.last_name) as `Actor Name`
+FROM film_actor as fa
+INNER JOIN actor as a ON fa.actor_id = a.actor_id
+WHERE film_id = (SELECT film_id FROM film WHERE UCASE(title) = UCASE("Alone Trip"));
+
+-- 7c. You want to run an email marketing campaign in Canada, 
+-- for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+
+SELECT 
+	CONCAT_WS(" ", cust.first_name, cust.last_name) as `Customer Name`,
+    cust.email as `Email Address`,
+	ctry.country as `Country`
+FROM 
+	customer as cust
+INNER JOIN address as a 
+	ON cust.address_id = a.address_id
+INNER JOIN city
+	ON city.city_id = a.city_id
+INNER JOIN country as ctry
+	ON ctry.country_id = city.country_id
+WHERE UCASE(ctry.country) = UCASE("Canada");
+
+-- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+-- Identify all movies categorized as family films.
+
+SELECT
+	f.title as `Movie Name`,
+    c.name as `Category`
+FROM film as f
+INNER JOIN film_category as fc
+	ON f.film_id = fc.film_id
+INNER JOIN category as c
+	ON fc.category_id = c.category_id
+WHERE UCASE(c.name) = UCASE("Family");
+
+-- 7e. Display the most frequently rented movies in descending order.
+
+SELECT
+	f.title as `Movie`,
+    COUNT(r.rental_id) as `Times Rented`
+FROM rental as r
+INNER JOIN inventory as i
+	ON i.inventory_id = r.inventory_id
+INNER JOIN film as f
+	ON i.film_id = f.film_id
+GROUP BY f.film_id
+ORDER BY COUNT(r.rental_id) DESC;
+
+-- 7f. Write a query to display how much business, in dollars, each store brought in.
+SELECT
+	str.store_id as `Store`,
+    SUM(p.amount) as `Store Revenue ($)`
+FROM store as str
+INNER JOIN staff as stf 
+	ON str.store_id = stf.store_id
+INNER JOIN payment as p
+	ON stf.staff_id = p.staff_id
+GROUP BY str.store_id;
+
+-- 7g. Write a query to display for each store its store ID, city, and country.
+SELECT
+	s.store_id as `Store`,
+    city.city as `City Name`,
+    cntry.country as `Country`
+FROM store as s
+INNER JOIN address as a 
+	ON a.address_id = s.address_id
+INNER JOIN city
+	ON city.city_id = a.city_id
+INNER JOIN country as cntry
+	ON cntry.country_id = city.country_id;
